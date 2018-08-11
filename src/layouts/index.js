15e5a3a01a1c852744b7cb7e038de26ff149d90d
Javier Garcia <javier.garcia549@gmail.com>
2018-08-11 13:58:27 -0700

    works with markdown!

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,45 +1,64 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import Header from '../components/header'
-import './index.css'
+import './index.scss'
 
-const Layout = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header siteTitle={data.site.siteMetadata.title} />
+const Header = () => (
+  <div
+    style={{
+      background: 'rebeccapurple',
+      marginBottom: '1.45rem',
+    }}
+  >
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        padding: '1.45rem 1.0875rem',
       }}
     >
-      {children()}
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+          }}
+        >
+          Gatsby LUT
+        </Link>
+      </h1>
     </div>
   </div>
 )
 
-Layout.propTypes = {
+const TemplateWrapper = ({children}) => (
+    <div>
+      <Helmet
+        title="Gatsby Default Starter"
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      />
+      <Header />
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0,
+        }}
+      >
+        {children()}
+      </div>
+    </div>
+  )
+
+TemplateWrapper.propTypes = {
   children: PropTypes.func,
 }
 
-export default Layout
-
-export const query = graphql`
-  query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+export default TemplateWrapper
