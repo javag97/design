8b78f7d5bef2a27472691166d0e79f1aee5ba545
Javier Garcia <javier.garcia549@gmail.com>
2018-08-29 13:19:58 -0700

    last min push

--- src/components/card.js
+++ src/components/card.js
@@ -7,14 +7,13 @@ import styles from "../styles/card.module.scss"
 function Card(props) {
   return(
     <div className = {styles.card}>
-      <div className = {styles.card__anchor}>
-        <div className = {styles.card__text}>
-          <h2>SLO DO CO TO GO</h2>
-          <p>Connecting Aerospace to high quality suppliers</p>
+        <div className = {styles.card__anchor}>
+          <h2 className = {styles.card__text}>SLO DO CO TO GO</h2>
+          <p className = {styles.card__text}>Connecting Aerospace to high quality suppliers</p>
         </div>
         <Button label="Read more" to="/second-page" />
       </div>
-    </div>
+
   );
 }
 
