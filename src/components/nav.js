df9d5add9ae1bf76e32777541a6bb3d31ea93851
Javier Garcia <javier.garcia549@gmail.com>
2018-09-20 22:05:00 -0700

    nav header added

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,15 +4,16 @@ import React from 'react'
 import Button from './button.js'
 import styles from "../styles/components/_nav.module.scss"
 
-
-
 function Nav(props){
   return(
     <div className = {styles.nav}>
-      <h2>Javier Garcia </h2>
-      <p>Designer & Developer from National City.</p>
-      <Button label="Contact Me" />
+      <h2 className = {styles.nav__name}>Javier Garcia </h2>
+      <div className = {styles.nav__description}>
+        <p>Designer & Developer from National City.</p>
+        <Button label="Contact Me" />
       </div>
+
+    </div>
   );
 }
 
