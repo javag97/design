1e4c65f6f29fb7fc958481eb5c46c772f9069739
Javier Garcia <javier.garcia549@gmail.com>
2018-09-30 18:32:06 -0700

    404 page is better, footer works better with grid

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,16 +9,20 @@ function Footer(props){
   return (
     <div className = {styles.footer} >
       <div className = {styles.footer__wrapper}>
+        <div>
         <h2 className = "text__white">Javier Garcia</h2>
         <p>© 2018 Javier Garcia</p>
+        </div>
         <ul>
           <li>HOME </li>
           <li>DATUM </li>
           <li>SLO DO CO TO GO </li>
           <li>CAMP POLYHACKS</li>
         </ul>
+        <div>
         <i class="fab fa-linkedin-in"></i>
         <i class="fab fa-facebook-f"></i>
+        </div>
         <Button label="Contact Me" />
         </div>
     </div>
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from '../components/button'
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
+    <h1>On darn..</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Button label="Return to safety" to="/" />
   </div>
 )
 
