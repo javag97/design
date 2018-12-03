19bb9de9a0a087d0ee0890ba14eceaaa0281361b
Javier Garcia <javier.garcia549@gmail.com>
2018-12-02 18:59:28 -0800

    removal of twitter graphql element

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -20,7 +20,6 @@ const SEO = ({ title, desc, banner, pathname, article }) => (
           pathPrefix,
           defaultDescription,
           defaultBanner,
-          twitter,
         },
       },
     }) => {
