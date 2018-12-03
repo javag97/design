d71e1bf0145512218e2eb5a844882d3a78037ac2
Javier Garcia <javier.garcia549@gmail.com>
2018-12-02 16:52:51 -0800

    template brought over

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,26 @@
-// Implement the Gatsby API “createPages”. This is called once the
-// data layer is bootstrapped to let plugins create pages from data.
+const path = require('path');
+
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
   return new Promise((resolve, reject) => {
-    const blogPostTemplate = path.resolve(`src/templates/post.js`)
-    // Query for markdown nodes to use in creating pages.
+    const postTemplate = path.resolve('src/templates/post.jsx');
+    const tagPage = path.resolve('src/pages/tags.jsx');
+    const tagPosts = path.resolve('src/templates/tag.jsx');
+
     resolve(
       graphql(
         `
-          {
-            allMarkdownRemark(limit: 1000) {
+          query {
+            allMarkdownRemark(
+              sort: { order: ASC, fields: [frontmatter___date] }
+            ) {
               edges {
                 node {
                   frontmatter {
                     path
+                    title
+                    tags
                   }
                 }
               }
@@ -23,23 +29,75 @@ exports.createPages = ({ graphql, actions }) => {
         `
       ).then(result => {
         if (result.errors) {
-          reject(result.errors)
+          return reject(result.errors);
         }
 
-        // Create pages for each markdown file.
-        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-          const path = node.frontmatter.path
+        const posts = result.data.allMarkdownRemark.edges;
+
+        const postsByTag = {};
+        // create tags page
+        posts.forEach(({ node }) => {
+          if (node.frontmatter.tags) {
+            node.frontmatter.tags.forEach(tag => {
+              if (!postsByTag[tag]) {
+                postsByTag[tag] = [];
+              }
+
+              postsByTag[tag].push(node);
+            });
+          }
+        });
+
+        const tags = Object.keys(postsByTag);
+
+        createPage({
+          path: '/tags',
+          component: tagPage,
+          context: {
+            tags: tags.sort(),
+          },
+        });
+
+        //create tags
+        tags.forEach(tagName => {
+          const posts = postsByTag[tagName];
+
+          createPage({
+            path: `/tags/${tagName}`,
+            component: tagPosts,
+            context: {
+              posts,
+              tagName,
+            },
+          });
+        });
+
+        //create posts
+        posts.forEach(({ node }, index) => {
+          const path = node.frontmatter.path;
+          const prev = index === 0 ? null : posts[index - 1].node;
+          const next =
+            index === posts.length - 1 ? null : posts[index + 1].node;
           createPage({
             path,
-            component: blogPostTemplate,
-            // In your blog post template's graphql query, you can use path
-            // as a GraphQL variable to query for data from the markdown file.
+            component: postTemplate,
             context: {
-              path,
+              pathSlug: path,
+              prev,
+              next,
             },
-          })
-        })
+          });
+        });
       })
-    )
-  })
-}
+    );
+  });
+};
+
+/* Allows named imports */
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
+  });
+};
