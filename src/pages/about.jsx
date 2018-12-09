d96366386488339eeb22ff6793ba5e8098443aed
Javier Garcia <javier.garcia549@gmail.com>
2018-12-09 00:37:47 -0800

    playing around with the about section, gotta add things to footer, content for start

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,21 +4,28 @@ import styled from 'react-emotion';
 import PropTypes from 'prop-types';
 import { Header } from 'components';
 import { Layout, Container } from 'layouts';
+import portrait from "../images/photo.jpg"
 
 const Text = styled.div`
-    margin: 2rem 10rem;
+  
+
+  img{
+    max-height: 300px;
   }
 `;
+
 
 const About = center => (
   <Layout>
     <Helmet title={'About Page'} />
     <Header title="About Page">Designer & Developer from San Diego.</Header>
     <Container center={center}>
+
       <Text>
+      <img src={portrait} alt="eyo" />
         <h3>
-        Born and raised in National City, California as a first generation Mexican-American.
-        I help purpose-driven organizations solve technology problems by using creativity and strategy to explore what works best for others. My hope is that others are inspired by such process to make a better society.	
+        Born and raised in National City (San Diego), California as a first generation Mexican American.
+        I help purpose-driven organizations solve technology problems by using creativity and problem solving to explore what works best for others. My hope is that others are inspired by such process to make a more functional society.	
         </h3>
       </Text>
     </Container>
